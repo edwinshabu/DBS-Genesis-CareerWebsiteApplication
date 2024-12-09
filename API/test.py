24a3582bc8de3b2f1bde25ed10d941c154ca90c3
Edwin Shabu Chakkalakkal <edwinshabu1998@gmail.com>
2024-12-09 21:11:42 +0000

    d

--- API/test.py
+++ API/test.py
@@ -1,9 +1,20 @@
 import unittest
 import base64
 import json
+from urllib import request
+
+from flask import Flask
+import requests
 from app import app
 import random
 import string
+
+
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+app.config['SESSION_TYPE'] = 'filesystem'  # Store sessions on the filesystem
+session = requests.Session()
+user_sessions = {}
+SESSION_TIMEOUT = 3000
 
 def generate_random_email(domain="gmail.com"):
     # Generate a random username with lowercase letters and digits
@@ -21,7 +32,7 @@ class TestRegisterAPI(unittest.TestCase):
         cls.app = app.test_client()
         cls.app.testing = True
 
-    def Check_If_User_Exist(self):
+    def test_Register_Check_If_User_Exist(self):
         # Prepare the username and password for authorization
         username = "andtrds"
         password = "Admin@123456"
@@ -58,7 +69,7 @@ class TestRegisterAPI(unittest.TestCase):
         # Assert the response status code
         self.assertEqual(response_data.get('message'), 'User exists!')
 
-    def Check_Missing_Fields(self):
+    def test_Register_Check_Missing_Fields(self):
         length = 8
         characters = string.ascii_lowercase + string.digits
         random_username = ''.join(random.choice(characters) for _ in range(length))
@@ -101,7 +112,7 @@ class TestRegisterAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 400)  # Bad Request for missing field
         self.assertEqual(response_data.get('message'), 'EmailId is required')
     
-    def Registration_Successful(self):
+    def test_Register_Successful(self):
         length = 8
         characters = string.ascii_lowercase + string.digits
         random_username = ''.join(random.choice(characters) for _ in range(length))
@@ -144,7 +155,48 @@ class TestRegisterAPI(unittest.TestCase):
         # Assert the response status code
         self.assertEqual(response_data.get('message'), 'Registration successful')
     
-    def Check_Contact_Validation(self):
+    def test_Register_AlreadyExist(self):
+        random_number = random.randint(1000000000, 9999999999)
+        random_email = generate_random_email()
+
+        # Prepare the username and password for authorization
+        username = 'pete'
+        password = "Admin@123456"
+        auth_string = f"{username}:{password}"
+        auth_base64 = base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
+
+        # Prepare headers with the Base64 encoded Authorization
+        headers = {
+            'Authorization': f'Basic {auth_base64}',
+        }
+
+        # Simulate a POST request to the /Register endpoint
+        with open('resume.pdf', 'rb') as resume, open('pic.jpg', 'rb') as pic:
+            response = self.app.post(
+                '/Register',
+                data={
+                    'FirstName': 'Tester',
+                    'LastName': 'User',
+                    'EmailId': f'{random_email}',  
+                    'ContactDetails': f'{random_number}',
+                    'SkillSet': 'Python, Flask',
+                    'UserType': 'Employee',
+                    'Organization': 'TechCorp',
+                    'Resume': resume,
+                    'ProfilePicture': pic
+                },
+                content_type='multipart/form-data',
+                headers=headers
+            )
+
+        # Parse the JSON response
+        response_data = json.loads(response.data.decode())
+
+        # Assert the response status code
+        self.assertEqual(response_data.get('message'), 'User exists!')
+    
+
+    def test_Register_Check_Contact_Validation(self):
         length = 8
         characters = string.ascii_lowercase + string.digits
         random_username = ''.join(random.choice(characters) for _ in range(length))
@@ -187,5 +239,53 @@ class TestRegisterAPI(unittest.TestCase):
         # Assert the response status code
         self.assertEqual(response_data.get('message'), 'Invalid contact number')
 
+    def test_Login_Check_Not_Registered(self):
+            username = 'admin'
+            password = 'Admin@123'  # Missing password field
+            
+            auth_string = f"{username}:{password}"
+            auth_base64 = base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
+            
+            # Prepare headers with the Base64 encoded Authorization
+            headers = {
+                'Authorization': f'Basic {auth_base64}',
+            }
+            
+            # Simulate a POST request to the /Login endpoint
+            response = self.app.post(
+                '/Login',
+                headers=headers
+            )
+
+            # Parse the JSON response
+            response_data = json.loads(response.data.decode())
+            print(response_data)
+
+            # Assert the response status code and error message
+            self.assertEqual(response_data.get('message'), "User is not registered!")
+
+    def test_Login_Check_Not_Registered(self):
+            username = 'admin'
+            password = 'Admin@123'  # Missing password field
+            
+            auth_string = f"{username}:{password}"
+            auth_base64 = base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
+            
+            # Prepare headers with the Base64 encoded Authorization
+            
+            # Simulate a POST request to the /Login endpoint
+            response = self.app.post(
+                '/Login'
+            )
+
+            # Parse the JSON response
+            response_data = json.loads(response.data.decode())
+            print(response_data)
+
+            # Assert the response status code and error message
+            self.assertEqual(response_data.get('message'), "Authentication header is missing.")
+        
+    
 if __name__ == '__main__':
     unittest.main()
+
